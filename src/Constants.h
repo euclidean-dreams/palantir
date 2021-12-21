9ae3b810640fd45cc1b4f69f6977808dd7cdf0c3
josiahdc <josiah.chapman@gmail.com>
2021-12-20 17:26:23 -0800

    post show commit

--- src/Constants.h
+++ src/Constants.h
@@ -2,8 +2,8 @@
 #define PALANTIR_CONSTANTS_H
 
 #define COSMOGRAPHER_ENDPOINT "ipc:///tmp/cosmographer_palantir"
-#define GLIMPSE_WIDTH 100
+#define GLIMPSE_WIDTH 60
 #define GLIMPSE_HEIGHT 74
-#define PIXEL_SIZE 10
+#define PIXEL_SIZE 12
 
 #endif //PALANTIR_CONSTANTS_H
