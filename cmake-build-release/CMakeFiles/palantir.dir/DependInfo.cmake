e26ffc0a5f07b6cb458aba81baed2b4ed5d8a74f
josiahdc <josiah.chapman@gmail.com>
2022-04-21 16:26:53 -0700

    added build directories

---
+++ cmake-build-release/CMakeFiles/palantir.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/josiah/projects/code/impresario-systems/palantir/src/Palantir.cpp" "CMakeFiles/palantir.dir/src/Palantir.cpp.o" "gcc" "CMakeFiles/palantir.dir/src/Palantir.cpp.o.d"
+  "/home/josiah/projects/code/impresario-systems/palantir/src/gizmo/Tchotchke.cpp" "CMakeFiles/palantir.dir/src/gizmo/Tchotchke.cpp.o" "gcc" "CMakeFiles/palantir.dir/src/gizmo/Tchotchke.cpp.o.d"
+  "/home/josiah/projects/code/impresario-systems/palantir/src/main.cpp" "CMakeFiles/palantir.dir/src/main.cpp.o" "gcc" "CMakeFiles/palantir.dir/src/main.cpp.o.d"
+  "/home/josiah/projects/code/impresario-systems/palantir/src/percipient/CosmographerPercipient.cpp" "CMakeFiles/palantir.dir/src/percipient/CosmographerPercipient.cpp.o" "gcc" "CMakeFiles/palantir.dir/src/percipient/CosmographerPercipient.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/josiah/projects/code/impresario-systems/palantir/cmake-build-release/impresario-utils-build/CMakeFiles/impresario-utils.dir/DependInfo.cmake"
+  "/home/josiah/projects/code/impresario-systems/palantir/cmake-build-release/yaml-build/CMakeFiles/yaml-cpp.dir/DependInfo.cmake"
+  "/home/josiah/projects/code/impresario-systems/palantir/cmake-build-release/spdlog-build/CMakeFiles/spdlog.dir/DependInfo.cmake"
+  "/home/josiah/projects/code/impresario-systems/palantir/cmake-build-release/flatbuffers-build/CMakeFiles/flatbuffers.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
